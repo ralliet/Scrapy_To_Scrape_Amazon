7566c8ff32bdbf1b4915cdc42f28444da7f712b4
Ruben Alliet <allietruben@gmail.com>
2018-06-01 23:08:30 +0200

    scrape

--- Scrapy_To_Scrape_Amazon/items.py
+++ Scrapy_To_Scrape_Amazon/items.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
+from scrapy.item import Item, Field
 
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/items.html
-
-import scrapy
-
-
-class ScrapyToScrapeAmazonItem(scrapy.Item):
+class AmazonItem(Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+
+    # name of image
+    Name = Field()
+    # imagepath on local file system
+    Path = Field()
+    # commodity url
+    Source = Field()
+    # deep learning feature of image
+    Feature = Field()
